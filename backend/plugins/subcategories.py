a799f2aea91a9b1e6d245836820c40d6fc52e01d
ByteCntrl <bytecntrl@gmail.com>
2023-04-09 17:25:32 +0200

    Update subcategories plugin

--- backend/plugins/subcategories.py
+++ backend/plugins/subcategories.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from tortoise.exceptions import IntegrityError
 
 from ..database import Subcategories
-from ..utils import TokenJwt, UnicornException, refresh_token, roles, token_jwt
+from ..utils import TokenJwt, UnicornException, roles, token_jwt
 
 
 router = APIRouter(
@@ -15,7 +15,7 @@ router = APIRouter(
 # all: get subcategories
 @router.get("/")
 async def get_subcategories(
-    token: TokenJwt = Depends(refresh_token)
+    token: TokenJwt = Depends(token_jwt)
 ):
     categories = await Subcategories.all().order_by("order").values()
 
@@ -29,7 +29,7 @@ async def get_subcategories(
 # all: get list subcategories
 @router.get("/list")
 async def get_list_subcategories(
-    token: TokenJwt = Depends(refresh_token)
+    token: TokenJwt = Depends(token_jwt)
 ):
     categories = [x["name"] for x in await Subcategories.all().values()]
 
@@ -52,15 +52,21 @@ async def add_subcategory(
     item: AddSubcategoriesItem,
     token: TokenJwt = Depends(token_jwt)
 ):
+    if not item.name:
+        raise UnicornException(
+            status=400,
+            message="Name missed"
+        )
+
     try:
         await Subcategories(name=item.name, order=item.order).save()
-
-        return {"error": False, "message": ""}
     except IntegrityError:
         raise UnicornException(
             status=400,
-            message="existing subcategories"
+            message="Existing subcategories"
         )
+    
+    return {"error": False, "message": ""}
 
 
 #admin: delete category
@@ -75,7 +81,7 @@ async def delete_subcategory(
     if not await subcategory.exists():
         raise UnicornException(
             status=404,
-            message="subcategory not exist"
+            message="Subcategory not exist"
         )
     
     await subcategory.delete()
